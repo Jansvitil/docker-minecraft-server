5498aee391149bb2bc8566f8347a6a5e8549c8de
Jansvitil <142812375+Jansvitil@users.noreply.github.com>
2025-05-04 20:50:09 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 set -e
 
+# 🗂️ Cesty
 DATA_PATH="/data"
 ZIP_NAME="full-volume-$(date +%F).zip"
 TMP_DIR="/tmp"
+
+# 🐙 GitHub repozitář pro zálohu (změň na své uživatelské jméno a název repa!)
 REPO="Jansvitil/zachrana-paulieho"
 
-cd $DATA_PATH
+# 💾 Zazipujeme celý obsah připojeného volume
+cd "$DATA_PATH"
 zip -r "$TMP_DIR/$ZIP_NAME" .
 
-cd $TMP_DIR
+# 🧹 Smažeme starý adresář, pokud existuje
+cd "$TMP_DIR"
+rm -rf backup-repo
+
+# ⬇️ Klonujeme repozitář a přesuneme se do něj
 git clone https://"$GITHUB_TOKEN"@github.com/$REPO backup-repo
 cd backup-repo
+
+# ➕ Přidáme ZIP soubor a commitneme
 cp "../$ZIP_NAME" .
 git config user.name "Railway Backup Bot"
 git config user.email "railway@backup.bot"
